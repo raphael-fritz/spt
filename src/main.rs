f11026e84829d08774e1eacd753f3b4b78aecec1
raphael-fritz <raphael-fritz@users.noreply.github.com>
2022-12-26 18:25:08 +0100

    add query for user playlists and tracks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rspotify::{
-    model::{AdditionalType, Country, Market},
+    model::{AdditionalType, Country, Market, PlayableItem},
     prelude::*,
     scopes, AuthCodeSpotify, Credentials, OAuth,
 };
@@ -70,17 +70,48 @@ fn main() {
     };
 
     let spotify = AuthCodeSpotify::new(creds, oauth);
-    let url = spotify.get_authorize_url(false).unwrap();
+    //let url = spotify.get_authorize_url(false).unwrap();
     //auth_with_fresh_token(&spotify, &url).await;
     auth_with_prev_token(&spotify);
 
+    let market = Some(Market::Country(Country::Austria));
+    //let market: Option<Market> = None;
+    //let additional_types = Some(&[AdditionalType::Episode]);
+    //let additional_types: Option<&[AdditionalType; 1]> = None;
+    let fields: Option<&str> = None;
+
     // Running the requests
     let user = spotify.current_user().expect("Couldn't get current user!");
+
+    let user_playlists = spotify.user_playlists(user.id.clone());
+    println!("User playlists:");
+    for (i, playlist) in user_playlists.enumerate() {
+        let playlist = playlist.unwrap();
+        println!("{}: {:?}:", i, playlist.name,);
+        for (i, track) in spotify
+            .playlist_items(playlist.id, fields, market)
+            .enumerate()
+        {
+            let track = track.unwrap().track.unwrap();
+            match track {
+                PlayableItem::Track(track) => {
+                    println!(
+                        "\t{}: {}-{}",
+                        i,
+                        track.artists.first().unwrap().name,
+                        track.name
+                    );
+                }
+                _ => (),
+            }
+        }
+    }
 
     let currently_playing = spotify.current_playing(
         Some(Market::Country(Country::Austria)),
         Some(&[AdditionalType::Episode]),
     );
+    let currently_playing = currently_playing.unwrap().unwrap().context;
     println!("Currently playing: {currently_playing:?}");
 
     let token_expiry = spotify
